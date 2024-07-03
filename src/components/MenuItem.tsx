a134d9f1c3eac1aea03d6dc390b888a62972dbc9
Sensei Stiven <strujillodev@gmail.com>
2024-07-03 02:13:23 -0500

    feat: create menu item component

---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,18 @@
+import type { MenuItem } from "../types";
+
+type MenuItemProps = {
+  item: MenuItem;
+};
+
+export default function MenuItem({ item }: MenuItemProps) {
+  return (
+    <>
+      <button
+        className="border-2 border-teal-400 w-full p-3 flex justify-between hover:bg-teal-200"
+      >
+        <p>{item.name}</p>
+        <p className="font-black">{item.price}</p>
+      </button>
+    </>
+  );
+}
