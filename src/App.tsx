322b7c3c49c2ef18b7fe42d3f3d225f458ac8da0
Sensei Stiven <strujillodev@gmail.com>
2024-07-03 23:16:56 -0500

    feat: save order

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { menuItems } from "./data/db";
 import useOrder from "./hooks/useOrder";
 
 function App() {
-  const { order, addItem, removeItem, tip, setTip } = useOrder();
+  const { order, tip, setTip, addItem, removeItem, placeOrder } = useOrder();
 
   return (
     <>
@@ -28,11 +28,17 @@ function App() {
         </div>
 
         <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
-          <OrderContents order={order} removeItem={removeItem} />
+          {order.length ? (
+            <>
+              <OrderContents order={order} removeItem={removeItem} />
 
-          <TipPercentageForm setTip={setTip} />
+              <TipPercentageForm setTip={setTip} tip={tip} />
 
-          <OrderTotals order={order} tip={tip} />
+              <OrderTotals order={order} tip={tip} placeOrder={placeOrder} />
+            </>
+          ) : (
+            <p className="text-center">La orden esta vacia</p>
+          )}
         </div>
       </main>
     </>
--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -25,11 +25,18 @@ export default function useOrder() {
     setOrder(order.filter((item) => item.id !== id));
   };
 
+  const placeOrder = () => {
+    console.log('guardando...');
+    setOrder([]);
+    setTip(0);
+  }
+
   return {
     order,
     tip,
     setTip,
     addItem,
     removeItem,
+    placeOrder,
   };
 }
